1651ceaebb3ee5c1822cd5691b76db27284c82a5
Mark Veerasingam <g00403931@atu.ie>
2023-03-30 11:59:06 +0100

    final commit

---
+++ src/main/java/ie/atu/test/driven/development/Calculator.java
@@ -0,0 +1,7 @@
+package ie.atu.test.driven.development;
+
+public class Calculator {
+    public int add(int first, int second) {
+        return (first + second);
+    }
+}
--- src/test/java/ie/atu/test/driven/development/CalculatorTest.java
+++ src/test/java/ie/atu/test/driven/development/CalculatorTest.java
@@ -2,9 +2,14 @@ package ie.atu.test.driven.development;
 
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTest {
-    @Test
-    void justAnExample(){
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class CalculatorTest {
+    Calculator myCalc;
+
+    @Test
+    public void testAdd(){
+        myCalc = new Calculator();
+        assertEquals(40, myCalc.add(20,20));
     }
 }
